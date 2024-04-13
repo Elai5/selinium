3c3fe006625e147220cd94e6984fff25c9dcb86d
Elai5 <yvettelaine007@gmail.com>
2024-04-13 10:27:57 +0300

    for loop to convert str to text on num of cookie clicked

--- cookieclickers.py
+++ cookieclickers.py
@@ -14,6 +14,8 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 cookie_id = "bigCookie"
 cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
 
 WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
@@ -34,6 +36,25 @@ while True:
     cookie.click()
     cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
     cookies_count = int(cookies_count.replace(",",""))
-    print(cookies_count)
+    # print(cookies_count)
+
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+        # if not int continue
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
+   
+        # we iterate through the number of products, if we can click on them we break the for loop.
+        # find the price, based on the number of cookie based on price then
+        # find product(cause it the button while text is the price tag)
+        # if we can aford any of the product, we click on them which breaks the loop
+        # ensure product type is a string
 
 
